8521d66b32a22768cb5f8fbceaeca50779ce525c
Marco Costalba <mcostalba@gmail.com>
2013-02-28 07:46:05 +0100

    More aggressive SEE pruning
    After the lucas patch, it seems this is a sensible parameter.
    
    bench: 4549796

--- src/search.cpp
+++ src/search.cpp
@@ -890,7 +890,7 @@ split_point_start: // At split points actual search starts from here
           }
 
           // Prune moves with negative SEE at low depths
-          if (   predictedDepth < 2 * ONE_PLY
+          if (   predictedDepth < 3 * ONE_PLY
               && pos.see_sign(move) < 0)
           {
               if (SpNode)
